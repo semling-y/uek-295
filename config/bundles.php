1fe489fe7cb090b49a6925f46e3b3ce930696d05
Yamato Semling <yamato.semling@bict-edu.ch>
2023-05-04 10:46:30 +0200

    build: configure database, worked on composer.json

--- config/bundles.php
+++ config/bundles.php
@@ -5,4 +5,8 @@ return [
     JMS\SerializerBundle\JMSSerializerBundle::class => ['all' => true],
     FOS\RestBundle\FOSRestBundle::class => ['all' => true],
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
+    Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
+    Twig\Extra\TwigExtraBundle\TwigExtraBundle::class => ['all' => true],
+    Doctrine\Bundle\DoctrineBundle\DoctrineBundle::class => ['all' => true],
+    Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle::class => ['all' => true],
 ];
