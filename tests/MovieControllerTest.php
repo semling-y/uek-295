1a48e6d0f76220c155a2b2a0309fbe25f7bd5461
Yamato Semling <yamato.semling@bict-edu.ch>
2023-05-11 13:27:12 +0200

    build: install test-pack for testing

---
+++ tests/MovieControllerTest.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Tests;
+
+use PHPUnit\Framework\TestCase;
+
+class MovieControllerTest extends TestCase
+{
+    public function testSomething(): void
+    {
+        $this->assertTrue(true);
+    }
+}
