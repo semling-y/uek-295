03f0e0d9a9552fb6355f51c2b845d16226d58c89
Yamato Semling <yamato.semling@bict-edu.ch>
2023-05-11 14:14:22 +0200

    feat: creat test method for movie PostMethod, install doctrine/doctrine-fixtures-bundle & dama/doctrine-test-bundle

--- config/bundles.php
+++ config/bundles.php
@@ -11,4 +11,6 @@ return [
     Doctrine\Bundle\DoctrineBundle\DoctrineBundle::class => ['all' => true],
     Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle::class => ['all' => true],
     Symfony\Bundle\MonologBundle\MonologBundle::class => ['all' => true],
+    Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
+    DAMA\DoctrineTestBundle\DAMADoctrineTestBundle::class => ['test' => true],
 ];
--- tests/MovieControllerTest.php
+++ tests/MovieControllerTest.php
@@ -2,10 +2,54 @@
 
 namespace App\Tests;
 
+use App\DTO\CreateUpdateMovie;
+use GuzzleHttp\Client;
 use PHPUnit\Framework\TestCase;
 
 class MovieControllerTest extends TestCase
 {
+
+    private static $client;
+
+    /**
+     *
+     * @return void
+     */
+    public static function setUpBeforeClass(): void
+    {
+        self::$client = new Client([
+            "base_uri" => "http://localhost:8000"
+        ]);
+    }
+
+    /**
+     *
+     * @return void
+     * @throws \GuzzleHttp\Exception\GuzzleException
+     */
+    public function testPost() {
+        $dto = new CreateUpdateMovie();
+        $dto->name = "Filmname";
+        $dto->description = "Eine coole Beschreibung";
+        $dto->agerest = 9;
+        $dto->rating = 5;
+        $dto->genre = 1;
+
+        //Build request for post method
+        $request = self::$client->request("POST", "api/movie",
+            [
+                "body" => json_encode($dto)
+            ]
+        );
+
+        //get response for post method
+        $response = json_decode($request->getBody());
+
+        //asert methods for actual test code
+        $this->assertTrue($request->getStatusCode() == 200);
+        $this->assertTrue($response == "Film wurde erstellt.");
+    }
+
     public function testSomething(): void
     {
         $this->assertTrue(true);
