b9ad003e1512ad1a2f5988f42f1fc995bac96441
Yamato Semling <yamato.semling@bict-edu.ch>
2023-05-12 08:29:37 +0200

    build: create index_test.php, feat: MovieControllerTest.php -> edit base_uri

--- tests/MovieControllerTest.php
+++ tests/MovieControllerTest.php
@@ -2,6 +2,7 @@
 
 namespace App\Tests;
 
+use App\DTO\CreateUpdateGenre;
 use App\DTO\CreateUpdateMovie;
 use GuzzleHttp\Client;
 use PHPUnit\Framework\TestCase;
@@ -19,7 +20,7 @@ class MovieControllerTest extends WebTestCase
     public static function setUpBeforeClass(): void
     {
         self::$client = new Client([
-            "base_uri" => "http://localhost:8000"
+            "base_uri" => "http://localhost:8000/index_test.php"
         ]);
 
         $client = self::createClient();
@@ -32,7 +33,64 @@ class MovieControllerTest extends WebTestCase
         self::$application->run(new StringInput("doctrine:fixtures:load"));
     }
 
+    /**
+     *
+     * @return void
+     * @throws \GuzzleHttp\Exception\GuzzleException
+     */
+    public function testPostMovie() {
+        $dto = new CreateUpdateMovie();
+        $dto->name = "Filmname";
+        $dto->description = "Eine coole Beschreibung";
+        $dto->agerest = 9;
+        $dto->rating = 5;
+        $dto->genre = 1;
 
+        //Build request for post method
+        $request = self::$client->request("POST", "api/movie",
+            [
+                "body" => json_encode($dto)
+            ]
+        );
+
+        //get response for post method
+        $response = json_decode($request->getBody());
+
+        //asert methods for actual test code
+        $this->assertTrue($request->getStatusCode() == 200);
+        $this->assertTrue($response == "Film wurde erstellt.");
+    }
+
+    public function testPostGenre() {
+        $dto = new CreateUpdateGenre();
+        $dto->genre = "Genrenname";
+
+        //Build request for post method
+        $request = self::$client->request("POST", "api/genre",
+            [
+                "body" => json_encode($dto)
+            ]
+        );
+
+        //get response for post method
+        $response = json_decode($request->getBody());
+
+        //asert methods for actual test code
+        $this->assertTrue($request->getStatusCode() == 200);
+        $this->assertTrue($response->Filmkategorie == "Genrenname");
+    }
+
+    public function testGetMovie() {
+        $request = self::$client->request("GET", "api/movie");
+
+        $this->assertTrue($request->getStatusCode() == 200);
+    }
+
+    public function testGetGenre() {
+        $request = self::$client->request("GET", "api/genre");
+
+        $this->assertTrue($request->getStatusCode() == 200);
+    }
 
     public function testSomething(): void
     {
